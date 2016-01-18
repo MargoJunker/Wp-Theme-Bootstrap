cb74bb4d49800b4ad5c37e8aa58d9b6aa3eb3bb6
Margo Junker <margo.junker@outlook.com>
2016-01-19 00:10:16 +0200

    Lecture 49: Amazing Plugin: Advanced Custom Fields [Delivers #111905529]

--- wordpress/wp-content/themes/bootstrap2wordpress/page-home.php
+++ wordpress/wp-content/themes/bootstrap2wordpress/page-home.php
@@ -9,8 +9,18 @@ $launch_price           = get_post_meta( 6, 'launch_price', true );
 $final_price            = get_post_meta( 6, 'final_price', true );
 $course_url             = get_post_meta( 6, 'course_url', true );
 $button_text            = get_post_meta( 6, 'button_text', true );
-$optin_text            = get_post_meta( 6, 'optin_text', true );
-$optin_button_text            = get_post_meta( 6, 'optin_button_text', true );
+$optin_text             = get_post_meta( 6, 'optin_text', true );
+$optin_button_text      = get_post_meta( 6, 'optin_button_text', true );
+
+// Advanced Custom Fields
+$income_feature_image   = get_field('income_feature_image');
+$income_section_title   = get_field('income_section_title');
+$income_section_description  = get_field('income_section_description');
+$income_section_reason_1_title   = get_field('income_section_reason_1_title');
+$income_section_reason_1_description   = get_field('income_section_reason_1_description');
+$income_section_reason_2_title   = get_field('income_section_reason_2_title');
+$income_section_reason_2_description   = get_field('income_section_reason_2_description');
+
 
 get_header(); ?>
 
@@ -69,18 +79,22 @@ get_header(); ?>
 <section id="boost-income">
     <div class="container">
         <div class="section-header">
-            <img src="<?php bloginfo('stylesheet_directory');?>/assets/img/icon-boost.png" alt="Chart">
-            <h2>How You Can Boost Your Income</h2>
+
+            <?php if( !empty($income_feature_image)   )   :   ?>
+            <img src="<?php echo $income_feature_image['url'] ?>" alt="<?php echo $income_feature_image['alt'] ?>">
+            <?php endif; ?>
+
+            <h2><?php echo $income_section_title ?></h2>
         </div><!-- section-header -->
-        <p class="lead">Whether you’re a freelance designer, entrepreneur, employee for a company, code hobbyist, or looking for a new career — this course gives you an immensely valuable skill that will enable you to either:</p>
+        <p class="lead"><?php echo $income_section_description ?></p>
         <div class="row">
             <div class="col-sm-6">
-                <h3>Make money on the side</h3>
-                <p>So you can save up for that Hawaiian vacation you’ve been wanting, help pay off your debt, your car, your mortgage, or simply just to have bonus cash laying around.</p>
+                <h3><?php echo $income_section_reason_1_title ?></h3>
+                <p><?php echo $income_section_reason_1_description ?></p>
             </div><!-- col -->
             <div class="col-sm-6">
-                <h3>Create a full-time income</h3>
-                <p>WordPress developers have options. Many developers make a generous living off of creating custom WordPress themes and selling them on websites like ThemeForest. Freelance designers and developers can also take on WordPress projects and make an extra $1,000 - $5,000+ per month.</p>
+                <h3><?php echo $income_section_reason_2_title ?></h3>
+                <p><?php echo $income_section_reason_2_description ?></p>
             </div><!-- col -->
         </div><!-- row -->
     </div><!-- container -->
