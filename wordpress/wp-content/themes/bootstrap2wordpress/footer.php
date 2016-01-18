0f30a24fde38b73f4a33b6f34a262b02fdd10acb
Margo Junker <margo.junker@outlook.com>
2016-01-18 22:38:06 +0200

    Lecture 46: Convert Your Static Footer to a Dynamic Footer [Delivers #111905445]

--- wordpress/wp-content/themes/bootstrap2wordpress/footer.php
+++ wordpress/wp-content/themes/bootstrap2wordpress/footer.php
@@ -11,18 +11,20 @@
 
 ?>
 
-	</div><!-- #content -->
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'bootstrap2wordpress' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'bootstrap2wordpress' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'bootstrap2wordpress' ), 'bootstrap2wordpress', '<a href="http://margojunkertest.eu" rel="designer">Margo Junker</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
 <?php wp_footer(); ?>
+
+<!-- Bootstrap core JavaScript
+     Placed at the end of the document so the pages
+     load faster!
+================================================== -->
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/jquery-2.1.1.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/bootstrap.min.js"></script>
+<script src="<?php bloginfo('template_directory'); ?>/assets/js/main.js"></script>
+
+<!-- TypeKit -->
+<script src="https://use.typekit.net/cvb8irl.js"></script>
+<script>try{Typekit.load({ async: true });}catch(e){}</script>
 
 </body>
 </html>
