a3ea9a349e5e7b57cf103cdc7f5a550d73aa36ca
Margo Junker <margo.junker@outlook.com>
2016-01-19 02:29:40 +0200

    Lecture 55: Code the Instructor Section with Advanced Custom Fields [Delivers #111905755]

--- wordpress/wp-content/themes/bootstrap2wordpress/page-home.php
+++ wordpress/wp-content/themes/bootstrap2wordpress/page-home.php
@@ -34,6 +34,18 @@ $project_feature_body = get_field('project_feature_body');
 
 $video_featurette_title = get_field('video_featurette_title');
 
+$instructor_section_title = get_field('instructor_section_title');
+$instructor_name = get_field('instructor_name');
+$bio_excerpt = get_field('bio_excerpt');
+$full_bio = get_field('full_bio');
+$twitter_username = get_field('twitter_username');
+$facebook_username = get_field('facebook_username');
+$google_username = get_field('google_username');
+$number_of_students = get_field('number_of_students');
+$number_of_reviews = get_field('number_of_reviews');
+$number_of_courses = get_field('number_of_courses');
+
+
 get_header(); ?>
 
 <!-- HERO
@@ -198,39 +210,42 @@ get_header(); ?>
             <div class="col-sm-8 col-md-6">
                 <div class="row">
                     <div class="col-lg-8">
-                        <h2>Your Instructor <small>Brad Hussey</small></h2>
+                        <h2><?php echo $instructor_section_title ?> <small><?php echo $instructor_name ?></small></h2>
                     </div><!-- col -->
                     <div class="col-lg-4">
-                        <a href="https://twitter.com/bradhussey" target="_blank" class="badge social twitter"><i class="fa fa-twitter"></i></a>
-                        <a href="https://facebook.com/bradhussey" target="_blank" class="badge social facebook"><i class="fa fa-facebook"></i></a>
-                        <a href="https://plus.google.com/+BradHussey" target="_blank" class="badge social gplus"><i class="fa fa-google-plus"></i></a>
+                        <?php if( !empty($twitter_username) ): ?>
+                        <a href="https://twitter.com/<?php echo $twitter_username ?>" target="_blank" class="badge social twitter"><i class="fa fa-twitter"></i></a>
+                        <?php endif; ?>
+                        <?php if( !empty($facebook_username) ): ?>
+                        <a href="https://facebook.com/<?php echo $facebook_username ?>" target="_blank" class="badge social facebook"><i class="fa fa-facebook"></i></a>
+                        <?php endif; ?>
+                        <?php if( !empty($google_username) ): ?>
+                        <a href="https://plus.google.com/<?php echo $google_username ?>" target="_blank" class="badge social gplus"><i class="fa fa-google-plus"></i></a>
+                        <?php endif; ?>
                     </div><!-- col -->
                 </div><!-- row -->
-                <p class="lead">A highly skilled professional, Brad Hussey is a passionate and experienced web designer, developer, blogger and digital entrepreneur.</p>
-                <p>Hailing from North Of The Wall (Yellowknife, Canada), Brad made the trek to the Wet Coast (Vancouver, Canada) to educate and equip himself with the necessary skills to become a spearhead in his trade of solving problems on the web, crafting design solutions, and speaking in code.</p>
-                <p>Brad's determination and love for what he does has landed him in some pretty interesting places with some neat people. He's had the privilege of working with, and providing solutions for, numerous businesses, big & small, across the Americas.</p>
-                <p>Brad builds custom websites, and provides design solutions for a wide-array of clientele at his company, Brightside Studios. He regularly blogs about passive income, living your life to the fullest, and provides premium quality web design tutorials and courses for tens of thousands of amazing people desiring to master their craft.</p>
+                <p class="lead"><?php echo $full_bio ?></p>
                 <hr>
                 <h3>The Numbers <small>They Don&rsquo;t Lie</small></h3>
                 <div class="row">
                     <div class="col-xs-4">
                         <div class="num">
                             <div class="num-content">
-                                41 000+ <span>students</span>
+                                <?php echo $number_of_students ?> <span>students</span>
                             </div><!-- num-content -->
                         </div><!-- num -->
                     </div><!-- col -->
                     <div class="col-xs-4">
                         <div class="num">
                             <div class="num-content">
-                                568 <span>reviews</span>
+                                <?php echo $number_of_reviews ?> <span>reviews</span>
                             </div><!-- num-content -->
                         </div><!-- num -->
                     </div><!-- col -->
                     <div class="col-xs-4">
                         <div class="num">
                             <div class="num-content">
-                                8 <span>courses</span>
+                                <?php echo $number_of_courses ?> <span>courses</span>
                             </div><!-- num-content -->
                         </div><!-- num -->
                     </div><!-- col -->
