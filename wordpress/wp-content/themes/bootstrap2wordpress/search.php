4dd3647f42b34400aada1607ed568a66ede7748d
Margo Junker <margo.junker@outlook.com>
2016-01-19 06:19:46 +0200

    Lecture 66: Customize the Search Results Template [Delivers #111906063]

--- wordpress/wp-content/themes/bootstrap2wordpress/search.php
+++ wordpress/wp-content/themes/bootstrap2wordpress/search.php
@@ -8,41 +8,45 @@
  */
 
 get_header(); ?>
+<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+	<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'bootstrap2wordpress' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+</section>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-		<?php
-		if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'bootstrap2wordpress' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
-			</header><!-- .page-header -->
+<div class="container">
+	<div id="primary" class="row">
+		<main id="content" class="col-sm-8">
 
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+			if ( have_posts() ) : ?>
 
-				/**
-				 * Run the loop for the search to output the results.
-				 * If you want to overload this in a child theme then include a file
-				 * called content-search.php and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', 'search' );
 
-			endwhile;
 
-			the_posts_navigation();
+				<?php
+				/* Start the Loop */
+				while ( have_posts() ) : the_post();
 
-		else :
+					/**
+					 * Run the loop for the search to output the results.
+					 * If you want to overload this in a child theme then include a file
+					 * called content-search.php and that will be used instead.
+					 */
+					get_template_part( 'content', 'search' );
 
-			get_template_part( 'template-parts/content', 'none' );
+				endwhile;
 
-		endif; ?>
+				the_posts_navigation();
 
-		</main><!-- #main -->
-	</section><!-- #primary -->
+			else :
 
-<?php
-get_sidebar();
-get_footer();
+				get_template_part( 'content', 'search' );
+
+			endif; ?>
+		</main>
+		<aside class="col-sm-4">
+			<?php get_sidebar(); ?>
+		</aside>
+	</div>
+</div>
+
+
+<?php get_footer(); ?>
