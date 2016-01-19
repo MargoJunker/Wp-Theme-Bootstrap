f6a541e0754d2030c3f4289c69c30d1609f1e0f5
Margo Junker <margo.junker@outlook.com>
2016-01-19 04:26:55 +0200

    Lecture 61: Easily Set Up Your Custom Blog Index Page [Delivers #111905931]

--- wordpress/wp-content/themes/bootstrap2wordpress/index.php
+++ wordpress/wp-content/themes/bootstrap2wordpress/index.php
@@ -13,44 +13,55 @@
  */
 
 get_header(); ?>
+	<section class="feature-image feature-image-default-alt" data-type="background" data-speed="2">
+		<h1 class="page-title">Blog</h1>
+	</section>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+	<!-- BLOG CONTENT
+        ============================================ -->
+	<div class="container">
+		<div class="row" id="primary">
 
-		<?php
-		if ( have_posts() ) :
+            <main id="content" class="col-sm-8" role="main">
+                <?php
+                if ( have_posts() ) :
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+                    if ( is_home() && ! is_front_page() ) : ?>
+                        <header>
+                            <h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
+                        </header>
 
-			<?php
-			endif;
+                        <?php
+                    endif;
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                    /* Start the Loop */
+                    while ( have_posts() ) : the_post();
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
+                        /*
+                         * Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        get_template_part( 'template-parts/content', get_post_format() );
 
-			endwhile;
+                    endwhile;
 
-			the_posts_navigation();
+                    the_posts_navigation();
 
-		else :
+                else :
 
-			get_template_part( 'template-parts/content', 'none' );
+                    get_template_part( 'template-parts/content', 'none' );
 
-		endif; ?>
+                endif; ?>
+            </main><!-- /content -->
+            <!-- SIDEBAR
+        ============================================ -->
+            <aside class="col-sm-4">
+                <?php get_sidebar(); ?>
+            </aside>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
+        </div><!-- /row -->
+    </div><!-- /container -->
+
+<?php get_footer(); ?>
